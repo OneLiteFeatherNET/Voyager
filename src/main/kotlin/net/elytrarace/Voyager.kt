0e91bfcab77bcf2e42a4c5d0e2a3317e88601787
TheMeinerLP <p.glanz@madfix.me>
2023-02-20 19:14:59 +0100

    Improve main class

--- src/main/kotlin/net/elytrarace/Voyager.kt
+++ src/main/kotlin/net/elytrarace/Voyager.kt
@@ -1,6 +1,24 @@
 package net.elytrarace
 
+import net.elytrarace.listener.BasicPlayerListener
+import net.elytrarace.listener.BasicWorldListener
+import net.elytrarace.service.CommandService
+import net.elytrarace.service.ConfigService
+import net.elytrarace.service.DatabaseService
+import net.elytrarace.service.MapService
 import org.bukkit.plugin.java.JavaPlugin
 
 class Voyager : JavaPlugin() {
+
+    lateinit var configService: ConfigService
+    lateinit var mapService: MapService
+    override fun onEnable() {
+        configService = ConfigService(this)
+        DatabaseService(this)
+        server.pluginManager.registerEvents(BasicWorldListener(), this)
+        server.pluginManager.registerEvents(BasicPlayerListener(), this)
+        mapService = MapService(this)
+        CommandService(this)
+    }
+
 }
