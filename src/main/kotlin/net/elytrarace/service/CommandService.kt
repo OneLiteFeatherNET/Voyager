6ab835ed3a2b33bcc2165394b2956b4bdcc9777a
TheMeinerLP <p.glanz@madfix.me>
2023-02-20 19:12:55 +0100

    Add command manager/service

---
+++ src/main/kotlin/net/elytrarace/service/CommandService.kt
@@ -0,0 +1,52 @@
+package net.elytrarace.service
+
+import cloud.commandframework.annotations.AnnotationParser
+import cloud.commandframework.arguments.parser.ParserParameters
+import cloud.commandframework.arguments.parser.StandardParameters
+import cloud.commandframework.bukkit.CloudBukkitCapabilities
+import cloud.commandframework.execution.CommandExecutionCoordinator
+import cloud.commandframework.meta.CommandMeta
+import cloud.commandframework.paper.PaperCommandManager
+import net.elytrarace.Voyager
+import net.elytrarace.commands.MapCommands
+import net.elytrarace.commands.RingCommands
+import org.bukkit.command.CommandSender
+import java.util.function.Function
+
+class CommandService(private val voyager: Voyager) {
+    private val paperCommandManager: PaperCommandManager<CommandSender> = PaperCommandManager(
+        voyager,
+        CommandExecutionCoordinator.simpleCoordinator(),
+        Function.identity(),
+        Function.identity()
+    )
+    private val annotationParser: AnnotationParser<CommandSender>
+    init {
+        if (paperCommandManager.hasCapability(CloudBukkitCapabilities.BRIGADIER)) {
+            paperCommandManager.registerBrigadier()
+            voyager.logger.info("Brigadier support enabled")
+        }
+        if (paperCommandManager.hasCapability(CloudBukkitCapabilities.ASYNCHRONOUS_COMPLETION)) {
+            paperCommandManager.registerAsynchronousCompletions()
+            voyager.logger.info("Asynchronous completions enabled")
+        }
+        val commandMetaFunction =
+            Function<ParserParameters, CommandMeta> { p: ParserParameters ->
+                CommandMeta.simple().with(
+                    CommandMeta.DESCRIPTION,
+                    p.get(StandardParameters.DESCRIPTION, "No description")
+                ).build()
+            }
+        annotationParser = AnnotationParser(
+            paperCommandManager,
+            CommandSender::class.java, commandMetaFunction
+        )
+        registerCommands()
+    }
+
+    private fun registerCommands() {
+        this.annotationParser.parse(MapCommands())
+        this.annotationParser.parse(RingCommands())
+    }
+
+}
