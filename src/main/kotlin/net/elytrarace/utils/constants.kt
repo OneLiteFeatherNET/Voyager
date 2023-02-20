1005d09b13d5e5f01468b66f8e8a0b7199f37aec
TheMeinerLP <p.glanz@madfix.me>
2023-02-20 19:14:36 +0100

    Add some constants

--- src/main/kotlin/net/elytrarace/utils/constants.kt
+++ src/main/kotlin/net/elytrarace/utils/constants.kt
@@ -1,3 +1,28 @@
 package net.elytrarace.utils
 
+import org.bukkit.Color
+import org.bukkit.Particle
+
+// Chat
 const val PREFIX = "<dark_gray>[</dark_gray><gradient:#fcba03:#03fc8c>ElytraRace</gradient><dark_gray>]</dark_gray> "
+const val COLOR_YELLOW_STRING = "#fcba03"
+const val COLOR_GREEN_STRING = "#03fc8c"
+
+
+// Config
+const val CONFIG_FILE_NAME = "config.conf"
+
+// Ring
+const val MINIMUM_POINT = 3
+
+// World
+const val VOID_GEN_STRING = "VoidGen"
+const val DEFAULT_WORLD = "world"
+
+// Scheduler
+const val SHOW_LINE_DELAY = 0L
+const val SHOW_LINE_TIMER = 10L
+const val SHOW_LINE_OFFSET = 0.0
+const val SHOW_LINE_COUNT = 1
+const val SHOW_LINE_EXTRA = .0
+val SHOW_LINE_PARTICLE = Particle.FLAME
