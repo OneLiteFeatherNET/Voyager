09afac61bfe7cbb655a0d0424bb798db14fc51a4
TheMeinerLP <p.glanz@madfix.me>
2023-04-30 16:50:36 +0200

    Add option to read lobby world file

--- src/main/kotlin/net/elytrarace/service/ConfigService.kt
+++ src/main/kotlin/net/elytrarace/service/ConfigService.kt
@@ -2,17 +2,32 @@ package net.elytrarace.service
 
 import com.typesafe.config.ConfigFactory
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.hocon.Hocon
 import kotlinx.serialization.hocon.decodeFromConfig
+import kotlinx.serialization.json.Json
 import net.elytrarace.Voyager
+import net.elytrarace.config.LobbyWorld
 import net.elytrarace.config.SQLConfig
 import net.elytrarace.utils.CONFIG_FILE_NAME
+import net.elytrarace.utils.LOBBY_WORLD_FILE_NAME
 import java.nio.file.Files
 import kotlin.io.path.reader
 
 class ConfigService(private val voyager: Voyager) {
     val config: SQLConfig
         get() = readConfig(voyager)
+    val lobbyWorld: LobbyWorld?
+        get() = readLobbyWorld(voyager)
+
+    private fun readLobbyWorld(voyager: Voyager): LobbyWorld? {
+        val world = voyager.server.worlds.firstOrNull {
+            val jsonFile = it.worldFolder.resolve(LOBBY_WORLD_FILE_NAME)
+            Files.exists(jsonFile.toPath())
+        } ?: return null
+        val jsonFile = world.worldFolder.resolve(LOBBY_WORLD_FILE_NAME)
+        return Json.decodeFromString<LobbyWorld?>(jsonFile.readText())?.copy(world = world)
+    }
 
     @OptIn(ExperimentalSerializationApi::class)
     private fun readConfig(voyager: Voyager): SQLConfig {
