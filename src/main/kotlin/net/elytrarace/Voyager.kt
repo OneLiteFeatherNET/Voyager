3b5688cf837e2eee51cdea21cc09a37ffdbf8330
TheMeinerLP <p.glanz@madfix.me>
2023-04-30 16:56:34 +0200

    Add another service

--- src/main/kotlin/net/elytrarace/Voyager.kt
+++ src/main/kotlin/net/elytrarace/Voyager.kt
@@ -2,9 +2,11 @@ package net.elytrarace
 
 import net.elytrarace.listener.BasicPlayerListener
 import net.elytrarace.listener.BasicWorldListener
+import net.elytrarace.listener.MapSelectorListener
 import net.elytrarace.service.CommandService
 import net.elytrarace.service.ConfigService
 import net.elytrarace.service.DatabaseService
+import net.elytrarace.service.InventoryService
 import net.elytrarace.service.MapService
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -12,11 +14,14 @@ class Voyager : JavaPlugin() {
 
     lateinit var configService: ConfigService
     lateinit var mapService: MapService
+    lateinit var inventoryService: InventoryService
     override fun onEnable() {
+        inventoryService = InventoryService()
         configService = ConfigService(this)
         DatabaseService(this)
         server.pluginManager.registerEvents(BasicWorldListener(), this)
-        server.pluginManager.registerEvents(BasicPlayerListener(), this)
+        server.pluginManager.registerEvents(BasicPlayerListener(this), this)
+        server.pluginManager.registerEvents(MapSelectorListener(this), this)
         mapService = MapService(this)
         CommandService(this)
     }
