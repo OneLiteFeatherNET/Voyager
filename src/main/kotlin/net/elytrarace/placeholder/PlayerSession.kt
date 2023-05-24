477cdd6aabcef358bd0e3ec77acd6c11dfeefb0b
TheMeinerLP <p.glanz@madfix.me>
2023-05-24 12:33:52 +0200

    Rename class

--- src/main/kotlin/net/elytrarace/placeholder/PlayerSession.kt
+++ src/main/kotlin/net/elytrarace/placeholder/PlayerSession.kt
@@ -2,9 +2,12 @@ package net.elytrarace.placeholder
 
 import org.bukkit.entity.Player
 import org.bukkit.inventory.Inventory
+import java.time.Instant
 
-data class PlayerProfile(
+data class PlayerSession(
     val currentPage: Int,
     val player: Player,
-    val mapSelectorInventory: Inventory
+    val mapSelectorInventory: Inventory,
+    val mapSession: MapSession,
+    val startTime: Instant?
 )
