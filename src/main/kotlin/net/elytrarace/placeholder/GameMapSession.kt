4fb43f3a968119e3bd077bc17ab47f4c5d781bae
TheMeinerLP <p.glanz@madfix.me>
2023-05-24 12:31:11 +0200

    Add map session holders

---
+++ src/main/kotlin/net/elytrarace/placeholder/GameMapSession.kt
@@ -0,0 +1,54 @@
+package net.elytrarace.placeholder
+
+import net.elytrarace.models.ElytraMap
+import net.elytrarace.models.Rings
+import net.elytrarace.utils.builder.itemBuilder
+import net.elytrarace.utils.extensions.interpolate
+import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.World
+import org.bukkit.inventory.ItemFlag
+import org.bukkit.inventory.ItemStack
+import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.text.MessageFormat
+
+
+class GameMapSession(
+    world: World,
+    val elytraMap: ElytraMap,
+    val splineLocations: List<Location> = calculateSpline(elytraMap, world),
+    val displayItem: ItemStack = buildDisplayItem(elytraMap)
+) : MapSession(world) {
+}
+
+private fun calculateSpline(elytraMap: ElytraMap, world: World): List<Location> = transaction {
+    val ringLocations = elytraMap.rings.orderBy(Rings.index to SortOrder.DESC)
+        .mapNotNull { it.locations.firstOrNull { center -> center.center }?.bukkitLocation }.windowed(6)
+    val firstSix = (ringLocations.last() + ringLocations.take(4).reduce { acc, locations -> acc + locations }) .toMutableList()
+    firstSix.add(world.spawnLocation)
+    val first = (interpolate(firstSix, 0, 300) + interpolate(firstSix, 2, 300))
+    val locs = ringLocations.map { locations ->
+        interpolate(locations, 0, 300) + interpolate(locations, 2, 300)
+    }
+    return@transaction first + (locs.reduce { acc, lists -> acc + lists })
+}
+
+private fun buildDisplayItem(elytraMap: ElytraMap): ItemStack = transaction {
+    println(elytraMap.displayName)
+    return@transaction itemBuilder {
+        material(Material.FILLED_MAP)
+        itemFlag(arrayOf(ItemFlag.HIDE_ITEM_SPECIFICS))
+        displayName(
+            MiniMessage.miniMessage()
+                .deserialize(MessageFormat.format("<#fcba03><!i>{0}", *arrayOf(elytraMap.displayName)))
+        )
+        lore(
+            arrayOf(
+                MiniMessage.miniMessage()
+                    .deserialize(MessageFormat.format("<#03fc8c><!i>{0}", *arrayOf(elytraMap.author)))
+            )
+        )
+    }
+}
