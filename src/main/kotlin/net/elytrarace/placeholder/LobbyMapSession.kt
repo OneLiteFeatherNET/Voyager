bbed830c9edea3b68b971b9abf75f88de8f15419
TheMeinerLP <p.glanz@madfix.me>
2023-05-24 12:32:33 +0200

    Add lobby session

---
+++ src/main/kotlin/net/elytrarace/placeholder/LobbyMapSession.kt
@@ -0,0 +1,7 @@
+package net.elytrarace.placeholder
+
+import net.elytrarace.config.LobbyWorld
+import org.bukkit.World
+
+class LobbyMapSession(world: World, val lobbyWorld: LobbyWorld) : MapSession(world) {
+}
