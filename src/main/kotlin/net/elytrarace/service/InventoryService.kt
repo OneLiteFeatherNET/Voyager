4e3b11c62f2f0cb476207403363dbdcd907561df
TheMeinerLP <p.glanz@madfix.me>
2023-05-24 12:36:07 +0200

    Improve inventory service

--- src/main/kotlin/net/elytrarace/service/InventoryService.kt
+++ src/main/kotlin/net/elytrarace/service/InventoryService.kt
@@ -1,28 +1,36 @@
 package net.elytrarace.service
 
-import net.elytrarace.utils.MAP_SELECTOR_INVENTORY_TITLE
+import net.elytrarace.Voyager
+import net.elytrarace.utils.BOOST_ITEM_NAME
+import net.elytrarace.utils.ELYTRA_ITEM_NAME
 import net.elytrarace.utils.MAP_SELECTOR_ITEM_NAME
 import net.elytrarace.utils.builder.itemBuilder
 import net.kyori.adventure.text.minimessage.MiniMessage
-import org.bukkit.Bukkit
 import org.bukkit.Material
-import org.bukkit.entity.Player
-import org.bukkit.inventory.Inventory
 
-class InventoryService {
+class InventoryService(
+    val voyager: Voyager
+) {
 
     val mapSelectorItem = itemBuilder {
         material(Material.PAPER)
         displayName(MiniMessage.miniMessage().deserialize(MAP_SELECTOR_ITEM_NAME))
     }
-    private val playerInventories = mutableMapOf<Player, Inventory>()
-
-    fun getPlayerInventory(player: Player): Inventory {
-        return playerInventories.getOrPut(player) {Bukkit.createInventory(null, 54, MiniMessage.miniMessage().deserialize(MAP_SELECTOR_INVENTORY_TITLE))}
+    val mapSelectorPlaceHolder = itemBuilder {
+        material(Material.BLACK_STAINED_GLASS_PANE)
+        displayName(MiniMessage.miniMessage().deserialize(" "))
     }
 
-    fun resetPlayer(player: Player) {
-        playerInventories.remove(player)
+    val elytraItem = itemBuilder {
+        material(Material.ELYTRA)
+        displayName(MiniMessage.miniMessage().deserialize(ELYTRA_ITEM_NAME))
     }
+
+    val boostItem = itemBuilder {
+        material(Material.FIREWORK_ROCKET)
+        displayName(MiniMessage.miniMessage().deserialize(BOOST_ITEM_NAME))
+    }
+
+    val itemsPerPage = voyager.mapService.mapSessions.windowed(4 * 9, 1, true)
 
 }
