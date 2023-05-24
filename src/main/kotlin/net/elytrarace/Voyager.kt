46f123a6bb8feb23e356bd04a1fd99f1635b59ea
TheMeinerLP <p.glanz@madfix.me>
2023-05-24 12:33:29 +0200

    Cleanup main

--- src/main/kotlin/net/elytrarace/Voyager.kt
+++ src/main/kotlin/net/elytrarace/Voyager.kt
@@ -3,11 +3,10 @@ package net.elytrarace
 import net.elytrarace.listener.BasicPlayerListener
 import net.elytrarace.listener.BasicWorldListener
 import net.elytrarace.listener.MapSelectorListener
-import net.elytrarace.service.CommandService
-import net.elytrarace.service.ConfigService
-import net.elytrarace.service.DatabaseService
-import net.elytrarace.service.InventoryService
-import net.elytrarace.service.MapService
+import net.elytrarace.listener.RaceBasicListener
+import net.elytrarace.service.*
+import net.elytrarace.utils.NAMESPACED_KEY
+import net.kyori.adventure.translation.TranslationRegistry
 import org.bukkit.plugin.java.JavaPlugin
 
 class Voyager : JavaPlugin() {
@@ -15,15 +14,31 @@ class Voyager : JavaPlugin() {
     lateinit var configService: ConfigService
     lateinit var mapService: MapService
     lateinit var inventoryService: InventoryService
+    lateinit var playerService: PlayerService
+    val translationRegistry: TranslationRegistry = TranslationRegistry.create(NAMESPACED_KEY)
     override fun onEnable() {
-        inventoryService = InventoryService()
         configService = ConfigService(this)
         DatabaseService(this)
+        mapService = MapService(this)
+        inventoryService = InventoryService(this)
+        playerService = PlayerService(this)
         server.pluginManager.registerEvents(BasicWorldListener(), this)
         server.pluginManager.registerEvents(BasicPlayerListener(this), this)
         server.pluginManager.registerEvents(MapSelectorListener(this), this)
-        mapService = MapService(this)
+        server.pluginManager.registerEvents(RaceBasicListener(this), this)
         CommandService(this)
+        /*val bundle = ResourceBundle.getBundle("test", Locale.US)
+        translationRegistry.registerAll(Locale.US, bundle.keySet()) {
+            val format = bundle.getString(it)
+            val alreadyDezelized = PlainTextComponentSerializer.plainText().serialize(MiniMessage.miniMessage().deserialize(TranslationRegistry.SINGLE_QUOTE_PATTERN.matcher(format).replaceAll("''")))
+            println(alreadyDezelized)
+            return@registerAll MessageFormat(
+                alreadyDezelized,
+                Locale.US
+            )
+        }
+        GlobalTranslator.translator().addSource(translationRegistry)*/
+
     }
 
 }
