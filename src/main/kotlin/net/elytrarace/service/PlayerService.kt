42386fa02feecd8b8e1e538822686d07cdb47d80
TheMeinerLP <p.glanz@madfix.me>
2023-05-25 21:34:32 +0200

    Add player inventory clear

--- src/main/kotlin/net/elytrarace/service/PlayerService.kt
+++ src/main/kotlin/net/elytrarace/service/PlayerService.kt
@@ -77,6 +77,7 @@ class PlayerService(
     }
 
     fun finishMap(playerSession: PlayerSession) {
+        playerSession.player.inventory.clear()
         val sb = playerSession.player.scoreboard
         sb.getObjective(OBJECTIVES_NAME)?.unregister()
         playerSession.player.scoreboard = sb
