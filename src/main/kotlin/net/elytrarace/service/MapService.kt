38eebad8a7515202c262cd98485fc691998a2c82
TheMeinerLP <p.glanz@madfix.me>
2023-05-25 18:11:29 +0200

    Add debug falling blocks

--- src/main/kotlin/net/elytrarace/service/MapService.kt
+++ src/main/kotlin/net/elytrarace/service/MapService.kt
@@ -1,12 +1,15 @@
 package net.elytrarace.service
 
 import net.elytrarace.Voyager
+import net.elytrarace.config.PluginMode
 import net.elytrarace.models.ElytraMap
 import net.elytrarace.placeholder.GameMapSession
 import net.elytrarace.placeholder.LobbyMapSession
 import net.elytrarace.utils.*
 import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.WorldCreator
+import org.bukkit.util.Vector
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class MapService(voyager: Voyager) {
@@ -22,6 +25,27 @@ class MapService(voyager: Voyager) {
             lobbyMapSession = TODO()
         }
         voyager.server.scheduler.runTaskTimerAsynchronously(voyager, showMapLines(), SHOW_LINE_DELAY, SHOW_LINE_TIMER)
+        if (voyager.configService.config.pluginMode == PluginMode.TESTING) {
+            transaction {
+                mapSessions.forEach { session ->
+                    session.elytraMap.rings.forEach {
+                        it.locations.forEach {
+                            val shulker = it.bukkitLocation.world.spawnFallingBlock(
+                                it.bukkitLocation.clone().add(0.5, 0.0, 0.5),
+                                Bukkit.createBlockData(Material.SAND)
+                            )
+                            shulker.isGlowing = true
+                            shulker.setGravity(false)
+                            shulker.dropItem = false
+                            shulker.velocity = shulker.velocity.add(Vector(0.0, 0.0, 0.0))
+                            shulker.ticksLived = Int.MAX_VALUE
+                            shulker.isVisibleByDefault = false
+                            shulker.shouldAutoExpire(false)
+                        }
+                    }
+                }
+            }
+        }
     }
 
     fun reloadActiveMaps() {
