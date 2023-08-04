deff99720cc078868e007a149727dd24e6578e4e
Phillipp Glanz <p.glanz@madfix.me>
2023-08-04 21:52:18 +0200

    Add database object.

---
+++ src/main/kotlin/net/elytrarace/model/dbo/ElytraMap.kt
@@ -0,0 +1,21 @@
+package net.elytrarace.model.dbo
+
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
+
+
+object ElytraMaps : IntIdTable() {
+    val name = varchar("name", 32)
+    val world = varchar("world", 32)
+    val displayName = varchar("displayName",32).nullable()
+    val author = varchar("author",32).nullable()
+}
+class ElytraMap(id: EntityID<Int>): IntEntity(id) {
+    companion object : IntEntityClass<ElytraMap>(ElytraMaps)
+    var name by ElytraMaps.name
+    var world by ElytraMaps.world
+    var displayName by ElytraMaps.displayName
+    var author by ElytraMaps.author
+}
