05dbdca40c0325780611e5537981faeea99a8219
Phillipp Glanz <p.glanz@madfix.me>
2023-08-04 22:52:33 +0200

    Add database service.

--- src/main/kotlin/net/elytrarace/Voyager.kt
+++ src/main/kotlin/net/elytrarace/Voyager.kt
@@ -1,8 +1,13 @@
 package net.elytrarace
 
+import net.elytrarace.service.ConfigService
 import org.bukkit.plugin.java.JavaPlugin
 
 class Voyager : JavaPlugin() {
+
+    val configService: ConfigService by lazy {
+        ConfigService(this)
+    }
     override fun onEnable() {
 
 
---
+++ src/main/kotlin/net/elytrarace/service/DatabaseService.kt
@@ -0,0 +1,32 @@
+package net.elytrarace.service
+
+import com.zaxxer.hikari.HikariDataSource
+import net.elytrarace.Voyager
+import net.elytrarace.logger.Voyager2SQLLogger
+import net.elytrarace.model.dbo.ElytraMaps
+import net.elytrarace.model.dbo.PortalLocations
+import net.elytrarace.model.dbo.Portals
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.DatabaseConfig
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.TransactionManager
+import org.jetbrains.exposed.sql.transactions.transaction
+
+class DatabaseService(private val voyager: Voyager) {
+    init {
+        val db = Database.connect(
+            HikariDataSource(voyager.configService.config.sqlConfig.toHikariConfig()),
+            databaseConfig = DatabaseConfig {
+                sqlLogger = Voyager2SQLLogger(voyager.getLogger())
+            }
+        )
+        TransactionManager.defaultDatabase = db
+        transaction {
+            SchemaUtils.createMissingTablesAndColumns(
+                ElytraMaps,
+                Portals,
+                PortalLocations
+            )
+        }
+    }
+}
