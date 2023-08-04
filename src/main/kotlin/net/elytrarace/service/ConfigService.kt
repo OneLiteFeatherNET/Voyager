dc7a83f003522bf655c70af10dd27f143b5627d5
Phillipp Glanz <p.glanz@madfix.me>
2023-08-04 22:49:21 +0200

    Add config service.

---
+++ src/main/kotlin/net/elytrarace/service/ConfigService.kt
@@ -0,0 +1,43 @@
+package net.elytrarace.service
+
+import com.typesafe.config.ConfigFactory
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.hocon.Hocon
+import kotlinx.serialization.hocon.decodeFromConfig
+import kotlinx.serialization.json.Json
+import net.elytrarace.Voyager
+import net.elytrarace.model.config.LobbyConfig
+import net.elytrarace.model.config.VoyagerConfiguration
+import net.elytrarace.utils.CONFIG_FILE_NAME
+import net.elytrarace.utils.LOBBY_WORLD_FILE_NAME
+import java.nio.file.Files
+import kotlin.io.path.reader
+
+class ConfigService(private val voyager: Voyager) {
+    val config: VoyagerConfiguration
+        get() = readConfig(voyager)
+    val lobbyWorld: LobbyConfig?
+        get() = readLobbyWorld(voyager)
+
+    private fun readLobbyWorld(voyager: Voyager): LobbyConfig? {
+        val world = voyager.server.worlds.firstOrNull {
+            val jsonFile = it.worldFolder.resolve(LOBBY_WORLD_FILE_NAME)
+            Files.exists(jsonFile.toPath())
+        } ?: return null
+        val jsonFile = world.worldFolder.resolve(LOBBY_WORLD_FILE_NAME)
+        return Json.decodeFromString<LobbyConfig?>(jsonFile.readText())?.copy(world = world)
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    private fun readConfig(voyager: Voyager): VoyagerConfiguration {
+        val sqlConfigFile = voyager.dataFolder.toPath().resolve(CONFIG_FILE_NAME)
+        if (!Files.exists(sqlConfigFile)) {
+            voyager.saveResource(CONFIG_FILE_NAME, true)
+        }
+        sqlConfigFile.reader().use { t ->
+            val config = ConfigFactory.parseReader(t)
+            return Hocon.decodeFromConfig(config)
+        }
+    }
+}
---
+++ src/main/kotlin/net/elytrarace/utils/constants.kt
@@ -0,0 +1,11 @@
+package net.elytrarace.utils
+
+// Config
+const val CONFIG_FILE_NAME = "config.conf"
+
+// Ring
+const val MINIMUM_POINT = 3
+
+// World
+const val VOID_GEN_STRING = "VoidGen"
+const val LOBBY_WORLD_FILE_NAME = "lobby.json"
