cc2dcebb0db18bf498f57e97a9c2435926183a45
Phillipp Glanz <p.glanz@madfix.me>
2023-08-06 00:35:24 +0200

    Improve plugin loading. Handles exceptions and failures

--- src/main/kotlin/net/elytrarace/Voyager.kt
+++ src/main/kotlin/net/elytrarace/Voyager.kt
@@ -1,8 +1,13 @@
 package net.elytrarace
 
 import net.elytrarace.listener.BasicWorldListener
+import net.elytrarace.model.config.CupConfiguration
+import net.elytrarace.model.dbo.Cup
+import net.elytrarace.model.dbo.Cups
+import net.elytrarace.model.dto.GameMapSession
 import net.elytrarace.phase.LinearPhaseSeries
 import net.elytrarace.phase.Phase
+import net.elytrarace.phases.GamePhase
 import net.elytrarace.phases.LobbyPhase
 import net.elytrarace.service.ConfigService
 import net.elytrarace.service.DatabaseService
@@ -11,7 +16,12 @@ import net.kyori.adventure.key.Key
 import net.kyori.adventure.translation.GlobalTranslator
 import net.kyori.adventure.translation.TranslationRegistry
 import net.kyori.adventure.util.UTF8ResourceBundleControl
+import org.bukkit.Bukkit
+import org.bukkit.WorldCreator
+import org.bukkit.WorldType
 import org.bukkit.plugin.java.JavaPlugin
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.*
 
 class Voyager : JavaPlugin() {
@@ -24,6 +34,18 @@ class Voyager : JavaPlugin() {
     val databaseService: DatabaseService by lazy {
         DatabaseService(this)
     }
+    val cup: Cup? by lazy {
+        getCup(configService.config.cupConfiguration)
+    }
+
+    val elytraPhase = LinearPhaseSeries<Phase>()
+    val playableMaps: MutableList<GameMapSession> = mutableListOf()
+
+    private fun getCup(cupConfiguration: CupConfiguration): Cup? = transaction {
+        val rowResult = Cups.select { Cups.name eq cupConfiguration.cupName }.firstOrNull() ?: return@transaction null
+        return@transaction Cup.wrapRow(rowResult)
+    }
+
     override fun onEnable() {
 
         val registry = TranslationRegistry.create(Key.key("voyager", "localization"))
@@ -33,11 +55,25 @@ class Voyager : JavaPlugin() {
         }
         registry.defaultLocale(supportedLocals.first())
         GlobalTranslator.translator().addSource(LynxWrapper(registry))
+        databaseService
 
         server.pluginManager.registerEvents(BasicWorldListener(), this)
-        val linearPhase = LinearPhaseSeries<Phase>()
-        linearPhase.add(LobbyPhase(this, cupConfiguration = configService.config.cupConfiguration))
-        linearPhase.start()
+        if (this.cup != null) {
+            elytraPhase.add(LobbyPhase(this, cupConfiguration = configService.config.cupConfiguration))
+            cup?.maps?.forEach {
+                val world = Bukkit.createWorld(WorldCreator.name(it.name).generator("VoidGen").type(WorldType.NORMAL))
+                if (world != null) {
+                    val gameMapSession = GameMapSession(world, it)
+                    playableMaps.add(gameMapSession)
+                    elytraPhase.add(GamePhase(this, gameMapSession))
+                }
+
+
+            }
+            elytraPhase.start()
+        }
+
+
     }
 
 }
