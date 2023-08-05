7a63adb8b7cb63472208470b2e85fed2f782c602
Phillipp Glanz <p.glanz@madfix.me>
2023-08-05 23:55:51 +0200

    Add i18n support. Only english support.

--- src/main/kotlin/net/elytrarace/Voyager.kt
+++ src/main/kotlin/net/elytrarace/Voyager.kt
@@ -3,9 +3,17 @@ package net.elytrarace
 import net.elytrarace.listener.BasicWorldListener
 import net.elytrarace.service.ConfigService
 import net.elytrarace.service.DatabaseService
+import net.elytrarace.utils.LynxWrapper
+import net.kyori.adventure.key.Key
+import net.kyori.adventure.translation.GlobalTranslator
+import net.kyori.adventure.translation.TranslationRegistry
+import net.kyori.adventure.util.UTF8ResourceBundleControl
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.*
 
 class Voyager : JavaPlugin() {
+
+    private val supportedLocals: Array<Locale> = arrayOf(Locale.US)
 
     val configService: ConfigService by lazy {
         ConfigService(this)
@@ -14,6 +22,15 @@ class Voyager : JavaPlugin() {
         DatabaseService(this)
     }
     override fun onEnable() {
+
+        val registry = TranslationRegistry.create(Key.key("voyager", "localization"))
+        supportedLocals.forEach { locale ->
+            val bundle = ResourceBundle.getBundle("voyager", locale, UTF8ResourceBundleControl.get())
+            registry.registerAll(locale, bundle, false)
+        }
+        registry.defaultLocale(supportedLocals.first())
+        GlobalTranslator.translator().addSource(LynxWrapper(registry))
+
         server.pluginManager.registerEvents(BasicWorldListener(), this)
     }
 
