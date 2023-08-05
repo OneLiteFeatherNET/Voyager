178b69aa29070c05419d2eb08e576b75854f7951
Phillipp Glanz <p.glanz@madfix.me>
2023-08-05 14:33:22 +0200

    Extend player model

--- src/main/kotlin/net/elytrarace/model/dto/ElytraPlayer.kt
+++ src/main/kotlin/net/elytrarace/model/dto/ElytraPlayer.kt
@@ -1,8 +1,15 @@
 package net.elytrarace.model.dto
 
+import net.elytrarace.model.dbo.Portal
 import org.apache.commons.geometry.euclidean.threed.Vector3D
-import java.util.concurrent.ArrayBlockingQueue
+import org.bukkit.entity.Player
+import java.time.Instant
 
 data class ElytraPlayer(
-    val positionQueue: ArrayList<Vector3D> = ArrayList()
+    val positionQueue: ArrayList<Vector3D> = ArrayList(),
+    val lastPortal: Portal? = null,
+    val startTime: Instant?,
+    val timeStampForRings: MutableMap<Portal, Instant> = mutableMapOf(),
+    val player: Player,
+    val mapSession: MapSession,
 )
