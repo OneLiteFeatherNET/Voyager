65e71b625278c8e3edbc4c1897951ea9ac4b3611
Phillipp Glanz <p.glanz@madfix.me>
2023-08-05 14:48:40 +0200

    Cleanup constant class.

--- src/main/kotlin/net/elytrarace/utils/constants.kt
+++ src/main/kotlin/net/elytrarace/utils/constants.kt
@@ -1,10 +1,12 @@
 package net.elytrarace.utils
 
+// Chat
+const val PREFIX = "<dark_gray>[</dark_gray><gradient:#fcba03:#03fc8c>ElytraRace</gradient><dark_gray>]</dark_gray> "
+const val COLOR_YELLOW_STRING = "#fcba03"
+const val COLOR_GREEN_STRING = "#03fc8c"
+
 // Config
 const val CONFIG_FILE_NAME = "config.conf"
-
-// Ring
-const val MINIMUM_POINT = 3
 
 // World
 const val VOID_GEN_STRING = "VoidGen"
