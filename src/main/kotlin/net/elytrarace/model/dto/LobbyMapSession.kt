537cd33f87e590eae0e5b71ecc8717be26abdfae
Phillipp Glanz <p.glanz@madfix.me>
2023-08-06 00:32:43 +0200

    Rename field

--- src/main/kotlin/net/elytrarace/model/dto/LobbyMapSession.kt
+++ src/main/kotlin/net/elytrarace/model/dto/LobbyMapSession.kt
@@ -1,7 +1,7 @@
 package net.elytrarace.model.dto
 
-import net.elytrarace.model.config.LobbyConfig
+import net.elytrarace.model.config.LobbyConfiguration
 import org.bukkit.World
 
-class LobbyMapSession(world: World, val lobbyConfig: LobbyConfig) : MapSession(world) {
+class LobbyMapSession(world: World, val lobbyConfiguration: LobbyConfiguration) : MapSession(world) {
 }
