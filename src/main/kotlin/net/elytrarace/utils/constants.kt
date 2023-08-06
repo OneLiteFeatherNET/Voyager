0ce316ae06f36e0597597c808ce55aba3c2badd3
Phillipp Glanz <p.glanz@madfix.me>
2023-08-06 04:52:58 +0200

    Add min point check magic value

--- src/main/kotlin/net/elytrarace/utils/constants.kt
+++ src/main/kotlin/net/elytrarace/utils/constants.kt
@@ -10,6 +10,9 @@ const val COLOR_GREEN_STRING = "#03fc8c"
 // Config
 const val CONFIG_FILE_NAME = "config.conf"
 
+// Portal
+const val MINIMUM_POINT = 3
+
 // World
 const val VOID_GEN_STRING = "VoidGen"
 const val LOBBY_WORLD_FILE_NAME = "lobby.json"
