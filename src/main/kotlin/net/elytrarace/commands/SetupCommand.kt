044cc98ecc73d16a9b6882c8c6e628a75a3cc61d
Phillipp Glanz <p.glanz@madfix.me>
2023-08-06 04:52:04 +0200

    Use other prompts

--- src/main/kotlin/net/elytrarace/commands/SetupCommand.kt
+++ src/main/kotlin/net/elytrarace/commands/SetupCommand.kt
@@ -2,13 +2,26 @@ package net.elytrarace.commands
 
 import cloud.commandframework.annotations.CommandMethod
 import net.elytrarace.Voyager
+import net.elytrarace.conversation.CupPrompt
+import net.elytrarace.conversation.MapPrompt
+import net.elytrarace.conversation.PortalPrompt
 import org.bukkit.entity.Player
 
 class SetupCommand(val voyager: Voyager) {
 
-    @CommandMethod("voyager setup")
-    fun setup(player: Player) {
-        voyager.setupService.startSetup(player)
+    @CommandMethod("voyager setup cup")
+    fun setupCup(player: Player) {
+        voyager.setupService.startSetup(player, CupPrompt())
+    }
+
+    @CommandMethod("voyager setup map")
+    fun setupMap(player: Player) {
+        voyager.setupService.startSetup(player, MapPrompt())
+    }
+
+    @CommandMethod("voyager setup portal")
+    fun setupPortal(player: Player) {
+        voyager.setupService.startSetup(player, PortalPrompt())
     }
 
 }
