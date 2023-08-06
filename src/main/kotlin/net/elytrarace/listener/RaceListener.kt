66a2213789d31881cd7ef29711327e777bb3194b
Phillipp Glanz <p.glanz@madfix.me>
2023-08-06 18:01:31 +0200

    Fix consume of boost items

--- src/main/kotlin/net/elytrarace/listener/RaceListener.kt
+++ src/main/kotlin/net/elytrarace/listener/RaceListener.kt
@@ -3,6 +3,8 @@ package net.elytrarace.listener
 import net.elytrarace.Voyager
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.event.player.PlayerItemConsumeEvent
 import org.bukkit.event.player.PlayerItemDamageEvent
 import org.bukkit.event.player.PlayerMoveEvent
 import org.bukkit.event.player.PlayerQuitEvent
@@ -23,6 +25,16 @@ class RaceListener(val voyager: Voyager) : Listener {
     @EventHandler
     fun handleItemDamage(event: PlayerItemDamageEvent) {
         this.voyager.inventoryService.handleItemDamageEvent(event)
+    }
+
+    @EventHandler
+    fun handleItemConsumeEvent(event: PlayerItemConsumeEvent) {
+        this.voyager.inventoryService.handleItemConsume(event)
+    }
+
+    @EventHandler
+    fun handleItemInteract(event: PlayerInteractEvent) {
+        this.voyager.inventoryService.handlePlayerInteract(event)
     }
 
 }
