cf49297480c9631b4a2936008bae12a9dfe1ea9d
Phillipp Glanz <p.glanz@madfix.me>
2023-08-06 19:26:59 +0200

    Improve lobby phase

--- src/main/kotlin/net/elytrarace/phases/LobbyPhase.kt
+++ src/main/kotlin/net/elytrarace/phases/LobbyPhase.kt
@@ -6,6 +6,7 @@ import net.elytrarace.phase.TickDirection
 import net.elytrarace.phase.TimedPhase
 import net.elytrarace.util.Strings
 import net.elytrarace.util.TimeFormat
+import net.kyori.adventure.sound.Sound
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.Bukkit
@@ -22,7 +23,22 @@ class LobbyPhase(val voyager: Voyager, private val cupConfiguration: CupConfigur
     override fun onUpdate() {
         val players = Bukkit.getOnlinePlayers()
         val time = Strings.getTimeString(TimeFormat.MM_SS, currentTicks)
+        val sound = when (currentTicks) {
+            60,
+            20,
+            15,
+            10,
+            5,
+            4,
+            3,
+            2,
+            1 -> Sound.sound { it.type(org.bukkit.Sound.BLOCK_NOTE_BLOCK_BASS).volume(2.0f) }
+            else -> null
+        }
         players.forEach {
+            if (sound != null) {
+                it.playSound(sound)
+            }
             it.sendActionBar(
                 Component.translatable("plugin.phase.lobby.current").args(Component.text(time))
             )
