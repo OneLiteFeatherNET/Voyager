4fd5f0529f65825e638de4f1f2e629c521b87142
Phillipp Glanz <p.glanz@madfix.me>
2023-08-06 04:52:18 +0200

    Handle bed enter event

--- src/main/kotlin/net/elytrarace/listener/BasicWorldListener.kt
+++ src/main/kotlin/net/elytrarace/listener/BasicWorldListener.kt
@@ -7,6 +7,7 @@ import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.block.LeavesDecayEvent
 import org.bukkit.event.player.PlayerArmorStandManipulateEvent
+import org.bukkit.event.player.PlayerBedEnterEvent
 
 class BasicWorldListener : Listener, CancellableListener {
     @EventHandler
@@ -14,6 +15,9 @@ class BasicWorldListener : Listener, CancellableListener {
 
     @EventHandler
     fun armorStandManipulateEvent(event: PlayerArmorStandManipulateEvent) = cancelling(event)
+
+    @EventHandler
+    fun bedEnterEvent(event: PlayerBedEnterEvent) = cancelling(event)
 
     @EventHandler
     fun blockBreak(event: BlockBreakEvent) = cancelling(event)
