0423eb57d35d49dd2b12a72ee081a7ab1b8c80ad
TheMeinerLP <p.glanz@madfix.me>
2023-08-07 00:16:57 +0200

    Add command permissions

--- src/main/kotlin/net/elytrarace/commands/LobbyCommand.kt
+++ src/main/kotlin/net/elytrarace/commands/LobbyCommand.kt
@@ -1,8 +1,8 @@
 package net.elytrarace.commands
 
 import cloud.commandframework.annotations.CommandMethod
+import cloud.commandframework.annotations.CommandPermission
 import net.elytrarace.Voyager
-import net.elytrarace.conversation.cup.CupPrompt
 import net.elytrarace.phases.LobbyPhase
 import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player
 class LobbyCommand(val voyager: Voyager) {
 
     @CommandMethod("voyager lobby force")
+    @CommandPermission("voyager.command.lobby.force")
     fun forceStart(player: Player) {
         val phase = voyager.elytraPhase.currentPhase
         if (phase is LobbyPhase) {
--- src/main/kotlin/net/elytrarace/commands/SetupCommand.kt
+++ src/main/kotlin/net/elytrarace/commands/SetupCommand.kt
@@ -1,6 +1,7 @@
 package net.elytrarace.commands
 
 import cloud.commandframework.annotations.CommandMethod
+import cloud.commandframework.annotations.CommandPermission
 import net.elytrarace.Voyager
 import net.elytrarace.conversation.cup.CupPrompt
 import net.elytrarace.conversation.map.MapPrompt
@@ -10,16 +11,19 @@ import org.bukkit.entity.Player
 class SetupCommand(val voyager: Voyager) {
 
     @CommandMethod("voyager setup cup")
+    @CommandPermission("voyager.command.setup.cup")
     fun setupCup(player: Player) {
         voyager.setupService.startSetup(player, CupPrompt())
     }
 
     @CommandMethod("voyager setup map")
+    @CommandPermission("voyager.command.setup.map")
     fun setupMap(player: Player) {
         voyager.setupService.startSetup(player, MapPrompt())
     }
 
     @CommandMethod("voyager setup portal")
+    @CommandPermission("voyager.command.setup.portal")
     fun setupPortal(player: Player) {
         voyager.setupService.startSetup(player, PortalPrompt())
     }
