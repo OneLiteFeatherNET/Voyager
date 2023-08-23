59e91ed1f9b54315ae52def3bb579e487476fcc6
Phillipp Glanz <p.glanz@madfix.me>
2023-08-23 22:34:29 +0200

    Remove testing dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,13 @@ plugins {
 }
 
 group = "net.onelitefeather"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1-SNAPSHOT" // Change
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/")
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
 }
 java {
     sourceCompatibility = JavaVersion.VERSION_17
