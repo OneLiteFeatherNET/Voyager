60c8794cfaa4233d0217153101779c552535ea33
TheMeinerLP <6745190+TheMeinerLP@users.noreply.github.com>
2024-09-27 00:55:58 +0200

    Add commands

--- plugins/game/src/main/java/net/elytrarace/game/ElytraRace.java
+++ plugins/game/src/main/java/net/elytrarace/game/ElytraRace.java
@@ -6,22 +6,39 @@ import net.elytrarace.api.phase.Phase;
 import net.elytrarace.common.cup.CupService;
 import net.elytrarace.common.cup.model.CupDTO;
 import net.elytrarace.common.map.MapService;
+import net.elytrarace.common.utils.PluginTranslationRegistry;
 import net.elytrarace.game.listener.DefaultListener;
 import net.elytrarace.game.phase.EndPhase;
 import net.elytrarace.game.phase.GamePhase;
 import net.elytrarace.game.phase.LobbyPhase;
 import net.elytrarace.game.phase.PreparationPhase;
 import net.elytrarace.game.world.VoidGenProvider;
-import org.apache.maven.model.building.Result;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.translation.GlobalTranslator;
+import net.kyori.adventure.translation.TranslationRegistry;
+import net.kyori.adventure.util.UTF8ResourceBundleControl;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.incendo.cloud.execution.ExecutionCoordinator;
+import org.incendo.cloud.paper.PaperCommandManager;
+import org.incendo.cloud.paper.util.sender.PaperSimpleSenderMapper;
+import org.incendo.cloud.paper.util.sender.PlayerSource;
+import org.incendo.cloud.paper.util.sender.Source;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Locale;
 import java.util.Optional;
+import java.util.ResourceBundle;
 import java.util.concurrent.CompletableFuture;
 import java.util.logging.Level;
 
@@ -32,6 +49,7 @@ public class ElytraRace extends JavaPlugin {
     private MapService mapService;
     private LinearPhaseSeries<Phase> elytraPhase;
     private CupDTO currentCup;
+    private PaperCommandManager<Source> commandManager;
 
     @Override
     public void onEnable() {
@@ -40,6 +58,11 @@ public class ElytraRace extends JavaPlugin {
         } catch (IOException e) {
             getLogger().warning("Unable to create plugin directory");
         }
+        this.registerLanguage();
+        this.commandManager = PaperCommandManager
+                .builder(PaperSimpleSenderMapper.simpleSenderMapper())
+                .executionCoordinator(ExecutionCoordinator.asyncCoordinator())
+                .buildOnEnable(this);
         this.elytraPhase = new LinearPhaseSeries<>();
         this.elytraPhase.add(new PreparationPhase(this));
         this.elytraPhase.add(new LobbyPhase(this));
@@ -49,6 +72,30 @@ public class ElytraRace extends JavaPlugin {
         getLogger().info("ElytraRace has been enabled!");
     }
 
+    private void registerLanguage() {
+        final TranslationRegistry translationRegistry = new PluginTranslationRegistry(TranslationRegistry.create(Key.key("elytrarace", "translations")));
+        translationRegistry.defaultLocale(Locale.US);
+        Path langFolder = getDataFolder().toPath().resolve("lang");
+        var languages = new HashSet<String>();
+        languages.add("en-US");
+        if (Files.exists(langFolder)) {
+            try (var urlClassLoader = new URLClassLoader(new URL[]{langFolder.toUri().toURL()})) {
+                languages.stream().map(Locale::forLanguageTag).forEach(r -> {
+                    var bundle = ResourceBundle.getBundle("bettergopaint", r, urlClassLoader, UTF8ResourceBundleControl.get());
+                    translationRegistry.registerAll(r, bundle, false);
+                });
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            languages.stream().map(Locale::forLanguageTag).forEach(r -> {
+                var bundle = ResourceBundle.getBundle("elytrarace", r, UTF8ResourceBundleControl.get());
+                translationRegistry.registerAll(r, bundle, false);
+            });
+        }
+        GlobalTranslator.translator().addSource(translationRegistry);
+    }
+
     @Override
     public void onDisable() {
         getLogger().info("ElytraRace has been disabled!");
@@ -56,6 +103,20 @@ public class ElytraRace extends JavaPlugin {
 
     private void registerCommands() {
         // Register commands here
+        commandManager.command(commandManager
+                .commandBuilder("voyager")
+                .literal("start")
+                .permission("elytrarace.command.start")
+                .senderType(PlayerSource.class)
+                .handler(context -> {
+                    Player player = context.sender().source();
+                    player.sendMessage(Component.translatable("phase.lobby.force"));
+                    Phase currentPhase = this.elytraPhase.getCurrentPhase().getCurrentPhase();
+                    if (currentPhase instanceof LobbyPhase lp) {
+                        lp.setCurrentTicks(20);
+                    }
+                })
+        );
     }
 
     private void registerListeners() {
